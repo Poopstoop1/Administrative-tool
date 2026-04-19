a5fa13820fd3f2251d9b14afb25a55930c955e50
Poopstoop1 <daniel.026@hotmail.com>
2026-04-18 23:12:36 -0300

    Refactor: Renomeado DashboardreponseDTO para DashboardResponseDTO

--- src/main/java/com/project/Mesa/Controller/dto/dashboard/DashboardResponseDTO.java
+++ src/main/java/com/project/Mesa/Controller/dto/dashboard/DashboardResponseDTO.java
@@ -4,13 +4,13 @@ import java.util.List;
 
 import com.project.Mesa.Controller.dto.campanha.CampanhaResponseDTO;
 
-public record DashboardReponseDTO(String message, List<CampanhaResponseDTO> campanhas,
+public record DashboardReponseDTO( List<CampanhaResponseDTO> campanhas,
 		List<CampanhaResponseDTO> campanhas1, List<CampanhaResponseDTO> campanhas2,
 		List<CampanhaResponseDTO> campanhas3, List<CampanhaResponseDTO> campanhas4) {
 
-	public DashboardReponseDTO FromDashBoardResponseDTO(String message, List<CampanhaResponseDTO> campanhas,
+	public DashboardReponseDTO FromDashBoardResponseDTO( List<CampanhaResponseDTO> campanhas,
 			List<CampanhaResponseDTO> campanhas1, List<CampanhaResponseDTO> campanhas2,
 			List<CampanhaResponseDTO> campanhas3, List<CampanhaResponseDTO> campanhas4) {
-		return new DashboardReponseDTO(message, campanhas, campanhas1, campanhas2, campanhas3, campanhas4);
+		return new DashboardReponseDTO(campanhas, campanhas1, campanhas2, campanhas3, campanhas4);
 	}
 }
