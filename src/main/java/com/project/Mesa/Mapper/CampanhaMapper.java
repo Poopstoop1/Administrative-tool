a9e8f07c6ba6e482ed69b5d80509a60835cb31d8
Poopstoop1 <daniel.026@hotmail.com>
2026-04-23 12:41:54 -0300

    Refactor: Ajustando CampanhaMapper

--- src/main/java/com/project/Mesa/Mapper/CampanhaMapper.java
+++ src/main/java/com/project/Mesa/Mapper/CampanhaMapper.java
@@ -1,11 +1,11 @@
 package com.project.Mesa.Mapper;
 
+import com.project.Mesa.Controller.dto.campanha.CampanhaRequestDTO;
 import com.project.Mesa.Controller.dto.campanha.CampanhaResponseDTO;
-import com.project.Mesa.Controller.dto.campanha.CampanhaSheetDTO;
 import com.project.Mesa.Model.Campanha;
 
 public interface CampanhaMapper {
-	Campanha toCampanha(CampanhaSheetDTO campanhaSheetDTO);
+	Campanha toCampanha(CampanhaRequestDTO campanhaSheetDTO);
 	
 	CampanhaResponseDTO campanhaResponseDTO(Campanha campanha);
 }
